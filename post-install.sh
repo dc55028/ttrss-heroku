6d984a7fbe56ecf6e29ad592d8d6b5d06f3626c3
Dom <dcomte@gmail.com>
2020-02-23 19:09:24 +0100

    still debugging post-install script

--- post-install.sh
+++ post-install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-git clone https://git.tt-rss.org/fox/tt-rss.git
-echo "PHP files in tt-rss: $(ls tt-rss/*.php | wc -l)"
+if [ "$DATABASE_URL" ]; then
+  git clone https://git.tt-rss.org/fox/tt-rss.git
 
-echo "Injecting configuration file..."
-cp ttrss-config.php tt-rss/config.php
+  echo "Injecting configuration file..."
+  cp ttrss-config.php tt-rss/config.php
 
-echo "Fixing permissions..."
-mkdir tt-rss/plugins.local tt-rss/cache tt-rss/lock tt-rss/feed-icons
-chmod -R -w tt-rss
-chmod +w tt-rss/plugins.local
-chmod -R 777 tt-rss/cache tt-rss/lock tt-rss/feed-icons
+  echo "Fixing permissions..."
+  chmod -R -w tt-rss
+  chmod +w tt-rss/plugins.local
+  chmod -R 777 tt-rss/cache tt-rss/lock tt-rss/feed-icons
 
-echo "Checking database..."
-if ! psql "$DATABASE_URL" -c 'SELECT schema_version FROM ttrss_version' &>/dev/null; then
-    echo "Initializing database..."
-    psql "$DATABASE_URL" < tt-rss/schema/ttrss_schema_pgsql.sql >/dev/null
+  echo "Checking database..."
+  if ! psql "$DATABASE_URL" -c 'SELECT schema_version FROM ttrss_version' &>/dev/null; then
+      echo "Initializing database..."
+      psql "$DATABASE_URL" < tt-rss/schema/ttrss_schema_pgsql.sql >/dev/null
+  fi
+
+  php plugins-installer.php
+else
+  echo "go add a Postgres database addon"
+  return 1
 fi
-
-php plugins-installer.php
